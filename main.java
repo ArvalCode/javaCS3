b93ef1551574a1ee6723eaf4b8ef9239039c5853
Nolan Tran <72712954+ArvalCode@users.noreply.github.com>
2023-09-19 15:55:38 +0000

    lkj

--- main.java
+++ main.java
@@ -16,8 +16,9 @@ public static void main(String[] args)
 
     switch (1) {
         case 1 : System.out.println("hi");
-        default : System.out.println("no");
+        case 2 : System.out.println("hi2");
         break;
+        default : System.out.println("no");
     }
 
     // Without break it'll keep running down the case statements (switch case and input must be same data type)
