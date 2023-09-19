3b3557a4a4dfd9c0206ae6f13273271c8ce8fb43
Nolan Tran <72712954+ArvalCode@users.noreply.github.com>
2023-09-19 15:50:17 +0000

    asdf

--- main.java
+++ main.java
@@ -12,4 +12,15 @@ public static void main(String[] args)
     // resultOfTernaryOperation = Integer.MIN_VALUE (anything)
     // (TF statement) firstOperand > secondOperand ? (only if true) firstOperand : (else) secondOperand;
 
+    // Switch variable and cases
+
+    switch (1) {
+        case 1 : System.out.println("hi");
+        default : System.out.println("no");
+        break;
+    }
+
+    // Without break it'll keep running down the case statements (switch case and input must be same data type)
+}
+
 }
