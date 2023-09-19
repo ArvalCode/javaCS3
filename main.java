f7894be8c00c633f2d607e2b1440f83bde125b02
Nolan Tran <72712954+ArvalCode@users.noreply.github.com>
2023-09-19 15:33:52 +0000

    changes

--- main.java
+++ main.java
@@ -1,1 +1,18 @@
 
+public class main
+{
+
+public static void main(String[] args)
+	{			    
+	// Show how to use do while statement by showing execution output
+    // Executes 1 time before exiting loop
+    // Do while objects???
+
+
+    // TenaryOperator: Goal is to show execution and output
+
+
+    // Integer = double.Bytes
+    // firstOperand > secondOperand ? firstOperand : secondOperand;
+
+}
